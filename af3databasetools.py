b87b511769e9ef727e209f9a80b3e8d4e5af341a
Jonas Hille <jonas.hille01@stud.uni-goettingen.de>
2026-01-25 15:54:00 +0100

    add functionality to extract paths to .cif structures + add functionality to create a folder ready to use as custom db with foldseek

--- af3databasetools.py
+++ af3databasetools.py
@@ -4,11 +4,21 @@ AF3DataBaseTools - A tool to make a folder structure usable for the Foldseek Cus
 """
 
 import argparse
+import os
 
-def extract_cif():
-    pass
+def extract_cif(input_folder):
+    cif_paths = []
+    for folder in os.listdir(input_folder):
+        for file in os.listdir(input_folder + "/" + folder):
+            if ".cif" in file:
+                cif_paths.append(input_folder + "/" + folder + "/" + file)
+    return cif_paths
 
-def create_db_folder():
+def create_db_folder(cif_paths, output_folder):
+    os.mkdir(output_folder)
+    for file in cif_paths:
+        cmd = f'cp "{file}" "{output_folder}"'
+        os.system(cmd)
     pass
 
 def main():
@@ -17,6 +27,9 @@ def main():
     argparser.add_argument("input_folder", help="Path to the Alphafold Outpuit")
     argparser.add_argument("output_folder", help="Path to the created db_folder")
     args = argparser.parse_args()
+
+    cif_paths = extract_cif(args.input_folder)
+    create_db_folder(cif_paths, args.output_folder)
 
 if __name__ == "__main__":
     main()
