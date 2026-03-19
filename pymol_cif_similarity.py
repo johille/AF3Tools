221f88d2203aa0a4d0e0a3056033eaeadfc785d7
Jonas Hille <jonas.hille01@stud.uni-goettingen.de>
2026-03-19 23:15:27 +0100

    implemented first iteration of compare mode WIP

--- pymol_cif_similarity.py
+++ pymol_cif_similarity.py
@@ -16,45 +16,52 @@ pymol_cif_similarity.py - A tool to acquire potential binding sites in cif molec
 def write_to_csv_whole(csv_path, pairs, result_pairs):
     result_map = {tuple(pair): dist_dict for pair, dist_dict in result_pairs}
 
-    global_sum = 0.0
-    global_n = 0
-
     with open(csv_path, "w", newline="") as f:
         w = csv.writer(f)
         w.writerow(["chain1", "resi1", "resn1", "chain2", "resi2", "resn2", "min_dist_A"])
 
         for chainA, chainB in pairs:
             dist_dict = result_map.get((chainA, chainB), {})
-
-            pair_sum = 0.0
-            pair_n = 0
-
             # sort by residue indices (resi are strings; if numeric, this is OK; otherwise lexicographic)
-            for (resiA, resnA, resiB, resnB), d in sorted(
-                dist_dict.items(),
-                key=lambda kv: (kv[0][0], kv[0][2])
-            ):
+            for (resiA, resnA, resiB, resnB), d in sorted(dist_dict.items(), key=lambda kv: (kv[0][0], kv[0][2])):
                 w.writerow([chainA, resiA, resnA, chainB, resiB, resnB, f"{d:.3f}"])
-                pair_sum += d
-                pair_n += 1
 
-            if pair_n > 0:
-                print(f"average binding distance {chainA}-{chainB}: {pair_sum/pair_n:.3f} Å ({pair_n} contacts)")
-                global_sum += pair_sum
-                global_n += pair_n
-                w.writerow([])
-            else:
-                print(f"no contacts within cutoff for {chainA}-{chainB}")
-
-    if global_n > 0:
-        print(f"global average binding distance: {global_sum/global_n:.3f} Å ({global_n} contacts)")
-    else:
-        print("no contacts within cutoff for any chain pair")
+            w.writerow([])
 
     print(f"Wrote combined CSV to {csv_path}")
 
+def get_average_dist(pairs, result_pairs):
+    result_map = {tuple(pair): dist_dict for pair, dist_dict in result_pairs}
 
-def single_result(cutoff, obj, chainA, chainB):
+    global_sum = 0.0
+    global_n = 0
+
+    for chainA, chainB in pairs:
+        dist_dict = result_map.get((chainA, chainB), {})
+
+        pair_n = 0
+        pair_sum = 0
+
+        pair_n = len(dist_dict)
+        pair_sum = sum(dist_dict.values())
+
+
+        if pair_n > 0:
+            print(f"average binding distance {chainA}-{chainB}: {pair_sum/pair_n:.3f} Å ({pair_n} contacts)")
+            global_sum += pair_sum
+            global_n += pair_n
+        else:
+            print(f"no contacts within cutoff for {chainA}-{chainB}")
+
+    if global_n > 0:
+        print(f"global average binding distance: {global_sum/global_n:.3f} Å ({global_n} contacts)")
+        return global_sum/global_n, global_n
+    else:
+        print("no contacts within cutoff for any chain pair")
+        return 0.0, 0.0
+
+
+def get_binding_pairs(cutoff, obj, chainA, chainB):
     selA = f"{obj} and chain {chainA}"
     selB = f"{obj} and chain {chainB}"
 
@@ -115,15 +122,46 @@ def single_facilitate(cif_path, cutoff, output_path):
     cmd.load(cif_path, obj)
     chains = cmd.get_chains(obj)
     pairs = list(combinations(chains, 2))
+
     result_pairs = []
     for pair in pairs:
-        result_pairs.append([pair, single_result(cutoff, obj, pair[0], pair[1])])
-    #for pair in result_pairs:
-        #write_to_csv(output_path, pair[0][0], pair[0][1], pair[1])
+        result_pairs.append([pair, get_binding_pairs(cutoff, obj, pair[0], pair[1])])
+
+    _ = get_average_dist(pairs, result_pairs)
     write_to_csv_whole(output_path, pairs, result_pairs)
-    #print(result_pairs)
+
     cmd.quit()
 
+def compare_facilitate(cif_path, cutoff, input_folder ,output_path):
+    ref_obj = "reference_structure"
+    cmd.load(cif_path, ref_obj)
+    ref_chains = cmd.get_chains(ref_obj)
+    ref_pairs = list(combinations(ref_chains, 2))
+    ref_result_pairs = []
+
+    for pair in ref_pairs:
+        ref_result_pairs.append([pair, get_binding_pairs(cutoff, ref_obj, pair[0], pair[1])])
+
+    
+    ref_avg_dist = get_average_dist(ref_pairs, ref_result_pairs)[0]
+    
+    input = os.listdir(input_folder)
+    avg_distances_structures = []
+    for structure in input:
+        if ".cif" in structure:
+            cmd.load(input_folder + "/" + structure, structure)
+            chains = cmd.get_chains(structure)
+            pairs = list(combinations(chains, 2))
+            result_pairs = []
+
+            for pair in pairs:
+                result_pairs.append([pair, get_binding_pairs(cutoff, structure, pair[0], pair[1])])
+            avg_dist = get_average_dist(pairs, result_pairs)[0]
+            diff = abs(ref_avg_dist - avg_dist)
+            avg_distances_structures.append([structure, avg_dist, diff])
+    avg_distances_structures.sort(key=lambda x: x[2])
+    print(avg_distances_structures)
+    pass
 
 def main():
     argparser = argparse.ArgumentParser(description="Pymol_Cif_Similarity - A tool to acquire potential binding sites in cif molecules and comparing multiple structures.")
@@ -136,15 +174,16 @@ def main():
 
     compare = mode.add_parser("compare")
     compare.add_argument("reference_cif")
-    compare.add_argument("input_folder")
+    compare.add_argument("--input_folder", type=str, required=True)
 
     args = argparser.parse_args()
 
     if args.cmd == "single":
         single_facilitate(args.cif, args.cutoff, args.output_path)
     elif args.cmd == "compare":
-        r_cif = args.reference_cif
-        input_folder = args.input_folder
+        compare_facilitate(args.reference_cif, args.cutoff, args.input_folder ,args.output_path)
+        #r_cif = args.reference_cif
+        #input_folder = args.input_folder
 
 if __name__ == "__main__":
     main()
